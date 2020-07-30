2e95a961506f89527b6bc8e3fdcd084f740c17cd
Patrizia M. Favaron <mafavaron@mac.com>
2020-07-30 12:07:12 +0200

    Update pbl_compare.f90
    Added skeleton of the Set member function

--- src/pbl_compare.f90
+++ src/pbl_compare.f90
@@ -12,5 +12,30 @@ module pbl_compare
     private
     
     ! Public interface
+    public  :: CompareType
+    
+    ! Main data type
+    type CompareType
+        real, dimension(:), allocatable :: rvPrimary
+        real, dimension(:), allocatable :: rvSecondary
+    contains
+        procedure   :: Set
+    end type CompareType
+    
+contains
+
+    function Set(this, rvP, rvS) result(iRetCode)
+    
+        ! Routine arguments
+        class(pbl_compare), intent(inout)   :: this
+        real, dimension(:), intent(in)      :: rvP
+        real, dimension(:), intent(in)      :: rvS
+        integer                             :: iRetCode
+        
+        ! Locals
+        integer     :: iErrCode
+        integer     :: iSize
+        
+    end function Set
 
 end module pbl_compare
