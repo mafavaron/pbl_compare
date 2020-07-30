1d3273fb938356fdddbfa773dc85e84920628fff
Patrizia M. Favaron <mafavaron@mac.com>
2020-07-30 12:30:44 +0200

    Update pbl_compare.f90
    Function "Set" completed

--- src/pbl_compare.f90
+++ src/pbl_compare.f90
@@ -27,7 +27,7 @@ contains
     function Set(this, rvP, rvS) result(iRetCode)
     
         ! Routine arguments
-        class(pbl_compare), intent(inout)   :: this
+        class(CompareType), intent(inout)   :: this
         real, dimension(:), intent(in)      :: rvP
         real, dimension(:), intent(in)      :: rvS
         integer                             :: iRetCode
@@ -35,6 +35,31 @@ contains
         ! Locals
         integer     :: iErrCode
         integer     :: iSize
+        
+        ! Assume success (will falsify on failure)
+        iRetCode = 0
+        
+        ! Check the input data is "correct", that is,
+        ! the two sets are non-empty and of same size
+        iSize = size(rvP)
+        if(iSize <= 0) then
+            iRetCode = 1
+            return
+        end if
+        if(iSize /= size(rvS)) then
+            iRetCode = 2
+            return
+        end if
+        ! Note the two tests, apparently not covering the whole intention,
+        ! in reality test it exhaistively.
+        
+        ! Clean-out and fill internal space
+        if(allocated(this % rvPrimary))   deallocate(this % rvPrimary)
+        if(allocated(this % rvSecondary)) deallocate(this % rvSecondary)
+        allocate(this % rvPrimary(iSize))
+        allocate(this % rvSecondary(iSize))
+        this % rvPrimary   = rvP
+        this % rvSecondary = rvS
         
     end function Set
 
