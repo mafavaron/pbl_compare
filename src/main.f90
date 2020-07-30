02375193d9c0428861a06ca9a5c959b811f50792
Patrizia M. Favaron <mafavaron@mac.com>
2020-07-30 11:01:32 +0200

    Create main.f90

---
+++ src/main.f90
@@ -0,0 +1,13 @@
+! Evaluation of standard validation indicators on some types of differences along a transect.
+!
+! This program is part of the pbl-compare project, and is covered by the MIT license.
+!
+! Author: Patrizia Favaron
+
+program GenerateIndicators
+
+    ! Loacls
+    
+
+end program GenerateIndicators
+
