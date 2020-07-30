d02b8bb1dbfba21caabdd064b2e58655f4ed4eee
Patrizia M. Favaron <mafavaron@mac.com>
2020-07-30 11:35:55 +0200

    Create pbl_compare.f90

---
+++ src/pbl_compare.f90
@@ -0,0 +1,16 @@
+! The pbl-compare library, and Fortran module.
+!
+! This is open source software, covered by the MIT license.
+!
+! Author: Patrizia Favaron
+
+
+module pbl_compare
+
+    implicit none
+    
+    private
+    
+    ! Public interface
+
+end module pbl_compare
