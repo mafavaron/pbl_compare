d8feec20bd09fa42c8eb256349bbc5d9a89d5f54
Patrizia M. Favaron <mafavaron@mac.com>
2020-08-06 12:12:44 +0200

    Update main.f90
    Addition

--- src/main.f90
+++ src/main.f90
@@ -9,19 +9,24 @@ program GenerateIndicators
     use pbl_compare
 
     implicit none
+    
+    ! Constants
+    integer, parameter  :: N_VARS   =     7
+    integer, parameter  :: N_CASES  =     4
+    integer, parameter  :: N        = 16384
 
     ! Locals
-    integer                 :: iRetCode
-    integer                 :: iVariant
-    integer                 :: iShiftVal
-    real                    :: rValue
-    real, dimension(16384)  :: rvPrimarySet
-    real, dimension(16384)  :: rvSecondarySet
-    type(CompareType)       :: tCmp
-    real                    :: rFB
-    real                    :: rDs
-    integer                 :: n = size(rvPrimarySet)
-    integer                 :: i
+    integer                             :: iRetCode
+    integer                             :: iVariant
+    integer                             :: iShiftVal
+    real                                :: rValue
+    real, dimension(N)                  :: rvPrimarySet
+    real, dimension(N)                  :: rvSecondarySet
+    real, dimension(N_VARS, N_CASES)    :: rmFB
+    type(CompareType)                   :: tCmp
+    real                                :: rFB
+    real                                :: rDs
+    integer                             :: i
     
     ! First case: Single peak of growing size
     rvPrimarySet   = 1.
@@ -60,14 +65,16 @@ program GenerateIndicators
     end do
     
     print *, "Norm ---------------------------------------------------"
-        
-    rvPrimarySet = [(exp(-(i-n/2)**2/2.e6),i=1,n)]
+    
+    open(10, file="FB.csv", status="unknown", action="write")
+    rvPrimarySet = [(exp(-(i-n/2)**2/2.e4),i=1,n)]
     do iShiftVal = -6000, 6000, 100
-        rvSecondarySet = [(exp(-(i-n/2+iShiftVal)**2/2.e6),i=1,n)]
+        rvSecondarySet = [(exp(-(i-n/2+iShiftVal)**2/2.e4),i=1,n)]
         iRetCode = tCmp % Set(rvPrimarySet, rvSecondarySet)
         rFB = tCmp % FB()
         print *, "    Fractional Bias (FB) = ", rFB, "  Shift = ", iShiftVal
     end do
+    close(10)
 
     print *, "==== ---------------------------------------------------"
         
